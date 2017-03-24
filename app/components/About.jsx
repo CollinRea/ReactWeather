57c3c4a7926d2a8df44800466bfb50866f7098b2
Collin Rea <collinr01@gmail.com>
2017-03-24 06:15:46 -0400

    Refactor app to use stateless react componenets and arrow functions

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 
-var About = React.createClass({
-  render: function () {
-    return (
-      <h3>About Component</h3>
-    );
-  }
-});
+// Example of Stateless functional Component
+// Creating component without using Class
+// must pass in props using this syntax
+
+var About = (props) => {
+  return (
+    <h3>About Component</h3>
+  )
+};
 
 module.exports = About;
