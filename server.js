433481fb92442fb86cae965c1153c9db1f1e1922
Collin Rea <collinr01@gmail.com>
2017-03-29 06:04:04 -0400

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 
 var app = express();
+// set port to pull from Environment Variable so server can run on Heroku
+var PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers('x-forwarded-proto') === 'http') {
+    next();
+  } else {
+    res.rediriect('http://'+req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function (){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function (){
+  console.log('Express server is up on port ' + PORT);
 });
